ee8f33dd0c4886878eeb8abfebe7bf62f5404eae
Sharkula347 <30978069+Sharkula347@users.noreply.github.com>
2020-04-01 20:31:36 +0300

    Update coll.py
    -Добавлен метод сравнения по пикселям - подсчитываются совпадающие и различающиеся пиксели.
    -Реализован алгоритм определения хозяина подписи(выводится его id)

--- coll.py
+++ coll.py
@@ -4,8 +4,10 @@ import os
 import numpy as np
 #import cbr as cb
 #Функция вычисления хэша
-def CalcImageHash(image,width,height):
+def CalcImageHash(image):
     '''Вычисление хэша'''
+    width = image.shape[1]
+    height = image.shape[0]
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #Переведем в черно-белый формат
     avg=gray_image.mean() #Среднее значение пикселя
     ret, threshold_image = cv2.threshold(gray_image, avg, 255, 0) #Бинаризация по порогу
@@ -48,7 +50,7 @@ def CornerCount(image):
 def img_cropper(image):
     '''Кадрирование изображения'''
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #Переведем в черно-белый формат
-    avg=gray_image.mean() #Среднее значение пикселя
+    avg=gray_image.mean()/koef #Среднее значение пикселя
     ret, threshold_image = cv2.threshold(gray_image, avg, 255, 0) #Бинаризация по порогу
     width=image.shape[1]
     height=image.shape[0]
@@ -81,6 +83,27 @@ def img_cropper(image):
         if val<255:
             break   
     return image[y:h,x:w]
+def MatchesCounter(img1,img2):
+    '''Подсчёт совпадающих пикселей(Размер картинок приведён к единому)'''
+    mcnt=0
+    diffcnt=0
+    gray_image = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)  # Переведем в черно-белый формат
+    avg = gray_image.mean() / koef  # Среднее значение пикселя
+    ret, threshold_image = cv2.threshold(gray_image, avg, 255, 0)  # Бинаризация по порогу
+
+    gray_image_2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)  # Переведем в черно-белый формат
+    avg_2 = gray_image.mean() / koef  # Среднее значение пикселя
+    ret, threshold_image_2 = cv2.threshold(gray_image_2, avg_2, 255, 0)  # Бинаризация по порогу
+    for x in range(threshold_image.shape[1]):
+        for y in range(threshold_image.shape[0]):
+            if threshold_image[x,y]==0 and threshold_image_2[x,y]==0:
+                mcnt=mcnt+1
+            if threshold_image[x,y]==255 and threshold_image_2[x,y]==0:
+                diffcnt=diffcnt+1
+            if threshold_image[x,y]==0 and threshold_image_2[x,y]==255:
+                diffcnt=diffcnt+1
+    return (mcnt,diffcnt) #/(threshold_image.shape[1]*threshold_image.shape[0])
+
 def CompareHash(hash1,hash2):
     '''Сравнение хэшей'''
     l=len(hash1)
@@ -91,30 +114,46 @@ def CompareHash(hash1,hash2):
             count=count+1
         i=i+1
     return count
-width=100
-height=100
+koef=1.25
+width=200
+height=200
 reals=[]
 forges=[]
-for root, dirs, files in os.walk("scr/real"):  
+#msgn real
+for root, dirs, files in os.walk("scr/msgn"):
     for filename in files:
-        reals.append("scr/real/" + filename)
-for a in reals[1:2]:
+        reals.append("scr/msgn/" + filename)
+for a in reals[6:7]:
+    Matches=0
+    Diffs=width*height
+    Mhash=Diffs
+    OwnerId=0
+    tmp=0
+    thash=0
     for b in reals:
         if a!=b:
             pic1=cv2.imread(a) #Прочитаем картинку
             pic2=cv2.imread(b) #Прочитаем картинку
             pic1 = cv2.resize(img_cropper(pic1), (width,height), interpolation = cv2.INTER_AREA) #Уменьшим картинку
             pic2 = cv2.resize(img_cropper(pic2), (width,height), interpolation = cv2.INTER_AREA) #Уменьшим картинку
-            hash1=CalcImageHash(pic1,width,height)
-            hash2=CalcImageHash(pic2,width,height)
+            tmp=MatchesCounter(pic1,pic2)
+            hash1 = CalcImageHash(pic1)
+            hash2 = CalcImageHash(pic2)
+            thash = CompareHash(hash1,hash2)
+            if tmp[0]>Matches:
+                Matches=tmp[0]
+                if thash < Mhash:
+                    Mhash=thash
+                    Diffs=width*height
+                    if tmp[1] < Diffs:
+                        Diffs=tmp[1]
+                        OwnerId=b.split("/")[-1][:3] #Записываем id хозяина подписи
             print(a+ " " + b)
             #print(hash1)
             #print(hash2)
-            print(str(CompareHash(hash1, hash2))+"| "+ str(CornerCount(pic1))+"| "+str(CornerCount(pic2)))
+            print("| Hash :"+str(thash)+"| Match: "+ str(tmp[0])+"| Diffs: "+ str(tmp[1])+"| Corners : "+ str(CornerCount(pic1) - CornerCount(pic2))+"|")
+    print("| ExitParans  "+ "| Hash :"+str(Mhash)+"| Match: "+ str(Matches)+"| Diffs: "+ str(Diffs)+"|")
+    print("OwnerId:" + OwnerId)
 
-gray_image = cv2.cvtColor(pic2, cv2.COLOR_BGR2GRAY) #Переведем в черно-белый формат
-avg=gray_image.mean() #Среднее значение пикселя
-ret, threshold_image = cv2.threshold(gray_image, avg, 255, 0) #Бинаризация по порогу
-cv2.imshow('Image with Borders', threshold_image)
 if cv2.waitKey(0) & 0xff == 27: 
     cv2.destroyAllWindows()
