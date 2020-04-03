1054f6b31b340328e7c7fe40dbcebfaea3aee10f
Sharkula347 <30978069+Sharkula347@users.noreply.github.com>
2020-04-03 16:50:29 +0300

    Update coll.py
    Программа подготовлена для загрузки подписей на проверку

--- coll.py
+++ coll.py
@@ -1,17 +1,14 @@
+#Казарян М.М., ВКБ23, Определение хозяина подписи
 import cv2
-import difflib
 import os
 import numpy as np
 
-
-# import cbr as cb
 # Функция вычисления хэша
 def CalcImageHash(image):
     '''Вычисление хэша'''
     width = image.shape[1]
     height = image.shape[0]
     threshold_image = binarize(image, 1)
-
     # Рассчитаем хэш
     _hash = ""
     for x in range(width):
@@ -23,7 +20,6 @@ def CalcImageHash(image):
                 _hash = _hash + "0"
 
     return _hash
-
 
 def CornerCount(image):
     '''Подсчёт углов'''
@@ -106,7 +102,6 @@ def MatchesCounter(img1, img2):
                 diffcnt = diffcnt + 1
     return (mcnt, diffcnt)  # /(threshold_image.shape[1]*threshold_image.shape[0])
 
-
 def CompareHash(hash1, hash2):
     '''Сравнение хэшей'''
     l = len(hash1)
@@ -118,17 +113,39 @@ def CompareHash(hash1, hash2):
         i = i + 1
     return count
 
+def owner_show(OwID):
+    '''Расшифровка ID'''
+    if os.path.exists("scr/key.txt"):
+        OwKey={}
+        f = open('scr/key.txt', 'r',encoding='utf-8')
+        for line in f:
+            line=line.replace('\n','').split(':')
+            OwKey.update({line[0]:line[1]})
+        f.close()
+        val=OwKey.get(OwID)
+        print("Расшифровка загружена успешно:\n"+str(OwKey)+"\n")
+        if val==None:
+            print("Данный id не содержится в расшифровке")
+            return OwID
+        else:
+            return val
+    else:
+        return val
 
 koef = 1.25
 width = 100
 height = 100
 reals = []
-forges = []
-# msgn real
-for root, dirs, files in os.walk("scr/real"):
+signs_to_identify = []
+signs_db_path="scr/real"# msgn real
+signs_tIdentify_path="scr/real"
+for root, dirs, files in os.walk(signs_db_path): #Пути к файлам из базы
     for filename in files:
-        reals.append("scr/real/" + filename)
-for a in reals[35:36]:
+        reals.append(signs_db_path+"/" + filename)
+for root, dirs, files in os.walk(signs_tIdentify_path): #Пути к файлам из базы
+    for filename in files:
+        signs_to_identify.append(signs_tIdentify_path+"/" + filename)
+for a in signs_to_identify[65:66]:
     Matches = 0
     Diffs = width * height
     Mhash = Diffs
@@ -137,8 +154,9 @@ for a in reals[35:36]:
     thash = 0
     pic1 = cv2.imread(a)  # Прочитаем картинку
     pic1 = cv2.resize(img_cropper(pic1), (width, height), interpolation=cv2.INTER_AREA)  # Уменьшим картинку
-    rex = reals.copy()
-    rex.remove(a)
+    if signs_tIdentify_path==signs_db_path:
+        rex = reals.copy()
+        rex.remove(a)
     for b in rex:
         pic2 = cv2.imread(b)  # Прочитаем картинку
         pic2 = cv2.resize(img_cropper(pic2), (width, height), interpolation=cv2.INTER_AREA)  # Уменьшим картинку
@@ -156,7 +174,7 @@ for a in reals[35:36]:
                     Diffs = tmp[1]
                     OwnerId = b.split("/")[-1][:3]
         print(a + " " + b)
-        print("| Hash :" + str(thash) + "| Match: " + str(tmp[0]) + "| Diffs: " + str(
+        print("| Хэш: " + str(thash) + "| Совпадения: " + str(tmp[0]) + "| Различия: " + str(
             tmp[1]) + "|")  # "| Corners : "+ str(tcorn)+"|")
-    print("| ExitParans  " + "| Hash :" + str(Mhash) + "| Match: " + str(Matches) + "| Diffs: " + str(Diffs) + "|")
-    print("OwnerId:" + OwnerId)
+    print("| Выходные значения:  " + "| Хэш: " + str(Mhash) + "| Совпадения: " + str(Matches) + "| Различия: " + str(Diffs) + "|")
+    print("Хозяин подписи {}: {}".format(a, owner_show(OwnerId)))
