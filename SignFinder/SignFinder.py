7bc5c5b918d46982b8ee7c69aa90b502f95ca5ec
Илья Краснокутский <bag2525@mail.ru>
2020-05-22 22:05:36 +0300

    *Добавлено нижнее меню -Консоль -Возможность выбора пути к файлам модели и json
    *Боковоее меню
    -Добавлена кнопка сравнить
    
    *Общие изменения
    -
    -Новый класс  Detector
    -Новая функция process_file (обработка изображения)
    -Новая функция load_model (загрузка модели)
    -Новая функция open_model_file (указание пути к модели)
    -Новая функцияopen_json_file (указание пути к json)

--- SignFinder/SignFinder.py
+++ SignFinder/SignFinder.py
@@ -9,7 +9,8 @@
 #------------------------------------------
 
 
-
+import cv2
+from imageai.Detection.Custom import CustomObjectDetection
 from tkinter import *
 from tkinter import filedialog as fd
 from PIL import ImageTk, Image
@@ -64,7 +65,8 @@ def open_file(arg):
     Считывание указанного (в проводнике) изображения, перерасчет его размеров и
     присваивание этого изображения объекту Label
 
-    :param arg: Список: [ссылка на объект рамки (Label), ширина рамки, высота рамки]
+    :param arg: Список: [ссылка на объект рамки (Label), ширина рамки, высота рамки,
+    ссылка на объект детектора (Detector)]
     :type arg:  list
 
     """
@@ -72,12 +74,16 @@ def open_file(arg):
     l_panel = arg[0]
     lable_width = arg[1]
     lable_height = arg[2]
+    detector = arg[3]
 
     global PIL_img
     global TK_img
 
     file_name = fd.askopenfilename(filetypes = (("PNG", "*.png"),
                                                 ("JPG", "*.jpg")))
+
+    #Передача ссылки на файл детектору
+    detector.image_path = file_name
 
     PIL_img = Image.open(file_name)
 
@@ -88,55 +94,240 @@ def open_file(arg):
     width_new = new_size[0]
     height_new = new_size[1]
 
-    PIL_img = PIL_img.resize((width_new, height_new), Image.ANTIALIAS)#
+    PIL_img = PIL_img.resize((width_new, height_new), Image.ANTIALIAS)
     TK_img = ImageTk.PhotoImage(PIL_img) 
     l_panel['image'] = TK_img
+
+
+def open_model_file(e_model_path):
+    """
+    Считывание указанного (в проводнике) файла модели и
+    присваивание этого изображения объекту Entry
+
+    :param arg: Объект типа Entry
+    :type arg:  Entry
+
+    """
+
+    file_name = fd.askopenfilename(filetypes = (("h5", "*.h5"), ("All files", "*.*")))
+
+    e_model_path.delete(0, END)
+    e_model_path.insert(0, file_name)
+
+
+def open_json_file(e_json_path): 
+    """
+    Считывание указанного (в проводнике) файла Json и
+    присваивание этого изображения объекту Entry
+
+    :param arg: Объект типа Entry
+    :type arg:  Entry
+
+    """
+
+    file_name = fd.askopenfilename(filetypes = (("JSON File", "*.json"), ("All files", "*.*")))
+
+    e_json_path.delete(0, END)
+    e_json_path.insert(0, file_name)
+
+def load_model(arg): 
+    """
+    Загрузка готовой модели.
+    Пути к файлам беруться из полей e_model_path и e_json_path
+
+    :param arg: Список: [ссылка на объект поля (model) (Entry), 
+    ссылка на объект поля (json) (Entry), ссылка на объект консоли (Text),
+    ссылка на объект detector (class Detector)]
+    :type arg:  list
+    """
+
+    e_model_path = arg[0]
+    e_json_path = arg[1]
+    t_console = arg[2]
+    detector = arg[3]
+
+    #Тег ошибки (красный цвет текста)
+    t_console.tag_config('warning', foreground="red")
+    #Тег успеха (зеленый цвет текста)
+    t_console.tag_config('success', foreground="green")
+
+    model_path = e_model_path.get()
+    json_path = e_json_path.get()
+
+    if ((len(model_path) == 0) or (len(json_path) == 0)):
+        t_console.insert(1.0, "\nНеверно указан путь к файлу Модели или к Json!", 'warning')
+        t_console.see("end")
+    else:
+        t_console.insert("end", "\nЗагрузка модели...", )
+        t_console.see("end")
+
+        try:      
+            detector.detector.setModelTypeAsYOLOv3()
+            detector.detector.setModelPath(model_path) #"detection_model-ex-008--loss-0003.909.h5"
+            detector.detector.setJsonPath(json_path)   #"detection_config.json"
+            detector.detector.loadModel()
+
+            t_console.insert("end", "\nМодель загружена!", 'success')
+            t_console.insert("end", "\nНазвание модели: " + model_path.split("/")[-1])
+            t_console.see("end")
+
+            detector.is_model_load = True
+        except Exception:
+            t_console.insert("end", "\nНе удалось загрузить модель!", 'warning')
+            t_console.see("end")
+    pass
+
+
+class Detector:
+    def __init__(self):
+        self.detector = CustomObjectDetection()
+        self.is_model_load = False
+        self.image_path = ""
+
+
+def process_file(arg):
+    """
+    Обработка изображения.
+
+    :param arg: Список: [ссылка на объект поля (model) (Entry), 
+    ссылка на объект поля (json) (Entry), ссылка на объект консоли (Text),
+    ссылка на объект detector (class Detector),
+    ссылка на объект t_console (Text)]
+    :type arg:  list
+    """
+
+    detector = arg[0]
+    l_panel_processed = arg[1]
+    WLABEL = arg[2]
+    HLABEL = arg[3]
+    t_console = arg[4]
+
+    #Тег ошибки (красный цвет текста)
+    t_console.tag_config('warning', foreground="red")
+    #Тег успеха (зеленый цвет текста)
+    t_console.tag_config('success', foreground="green")
+    
+    if (detector.is_model_load):
+        if (len(detector.image_path) != 0):
+
+            t_console.insert("end", "\nОбработка...", )
+            t_console.see("end")
+
+            global PIL_img_n
+            global TK_img_n
+
+            detections = detector.detector.detectObjectsFromImage(input_image = detector.image_path, output_image_path = "image000.jpg", minimum_percentage_probability = 10)
+            print(detections)
+    
+            # Вывод кординат и процентов совпадения подписей в консоль
+            for detection in detections:
+                t_console.insert("end", "\n" + str(detection["name"]) + ":" + str(detection["percentage_probability"]) + ":" + str(detection["box_points"]))
+            t_console.see("end")
+
+            PIL_img_n = Image.open("image000.jpg")
+
+            width_img = PIL_img_n.width
+            height_img = PIL_img_n.height
+
+            new_size = calculation_of_new_sizes(width_img, height_img, WLABEL, HLABEL)
+            width_new = new_size[0]
+            height_new = new_size[1]
+
+            PIL_img_n = PIL_img_n.resize((width_new, height_new), Image.ANTIALIAS)
+            TK_img_n = ImageTk.PhotoImage(PIL_img_n) 
+            l_panel_processed['image'] = TK_img_n
+
+            t_console.insert("end", "\nИзображение обработано!", 'success')
+            t_console.see("end")
+        else:
+            t_console.insert("end", "\nНе верно указан путь к изображению!", 'warning')
+            t_console.see("end")
+    else:
+        t_console.insert("end", "\nМодель не загружена!", 'warning')
+        t_console.see("end")
 
 
 
 def main():
     ''' Главная функция '''
 
+    #Инициализация обработчика
+    detector = Detector()
+
+    #Главное окно
     root = Tk()
-    root.geometry('1000x600') #'200x200'
+    root.geometry('1000x680')
+    root.configure(bg = '#dddddd')
     root.resizable(False, False) # Запрет на изменение размеров окна
     root.title("Finder")
 
+    #Создание меню в шапке
     main_menu = Menu(root) 
     root.config(menu = main_menu)
 
+    #Размер центральных окон
     WLABEL = 400
     HLABEL = 520
 
+
+    #Окна отображения изображений
     f_left = Frame(root)
+    f_left.configure(bg = '#dddddd')
 
+    #Окно отображающее не обработанное изображение
     f_source_img = LabelFrame(root, text = "Исходное изображение", width = 420, height = 520,)
-    f_processed_img = LabelFrame(root, text = "Обработанное изображение", width = 420, height = 520)
-
     l_panel_source = Label(root) 
-
-    label_test = Label(text = "123")
     
-    b_arg_list = [l_panel_source, WLABEL, HLABEL]
+    #Окно отображающее обработанное изображение
+    f_processed_img = LabelFrame(root, text = "Обработанное изображение", width = 420, height = 520)
+    l_panel_processed = Label(root) 
+
+
+    #Нижнее меню
+    #region down mwnu
+
+    #Консоль
+    t_console = Text(width = 50, height = 6)
+    scroll_console = Scrollbar(command = t_console.yview, orient="vertical")
+    t_console.config(yscrollcommand=scroll_console.set)
+
+    #Выбор пути к модели и json
+    l_model_path = Label(text = "Директория модели:")
+    l_json_path  = Label(text = "Директория json:")
+    e_model_path = Entry(width = 60)
+    e_json_path  = Entry(width = 60)
+    b_model_path = Button(root, text = '...', command = lambda arg = e_model_path: open_model_file(arg))
+    b_json_path  = Button(root, text = '...', command = lambda arg = e_json_path: open_json_file(arg))
+    b_load_model = Button(root, text = 'Загрузить модель', command = lambda arg = [e_model_path, e_json_path, t_console, detector]: load_model(arg))
+    l_model_path.configure(bg = '#dddddd')
+    l_json_path.configure(bg = '#dddddd')
+    #endregion
+
+
+    #Боковое меню
+    b_arg_list = [l_panel_source, WLABEL, HLABEL, detector]
     b_open_file = Button(f_left, text = 'Открыть', width = 10, command = lambda arg = b_arg_list: open_file(arg))
 
-    b_process = Button(f_left, text = 'Обработать', width = 10)
-    #b_open_file = Button(text = 'Открыть', command = lambda panel = l_panel: open_file(panel))
-    #b_open_file.bind('<Button-1>', lambda event, panel = l_panel: open_file(event, panel))
+    b_process = Button(f_left, text = 'Обработать', width = 10, command = lambda arg = [detector, l_panel_processed, WLABEL, HLABEL, t_console]: process_file(arg))
+    b_compare = Button(f_left, text = 'Сравнить', width = 10, )
 
-    f_source_img.grid(row = 0, column = 1, columnspan = 10, rowspan = 13, ipady=16, )
+
+    #Упаковка центральных окон
+    f_source_img.grid(row = 0, column = 1, columnspan = 10, rowspan = 13, ipady=16,)
     f_processed_img.grid(row = 0, column = 15, columnspan = 10, rowspan = 13, ipady=16,)
     l_panel_source.grid(row = 1, column = 1, columnspan = 10, rowspan = 13,)
+    l_panel_processed.grid(row = 0, column = 15, columnspan = 10, rowspan = 13,)
 
-    #b_open_file.grid(row = 0, column = 0, padx = 5, ipady = 5)
-    #b_process.grid(row = 1, column = 0, padx = 5, ipady = 5,)
 
-    f_left.grid(row = 0, column = 0, rowspan = 2, ipady=6, padx = 5)
+    #Упаковка бокового меню
+    f_left.grid(row = 0, column = 0, rowspan = 3, ipady=6, padx = 5)
     b_open_file.pack(pady = 1)
     b_process.pack(pady = 1)
+    b_compare.pack(pady = 1)
+   
     
+    #Меню в шапке
     #region Menu
-
     file_menu = Menu(main_menu, tearoff = 0)
     file_menu.add_command(label = "Открыть...", command = lambda arg = b_arg_list: open_file(arg))
     file_menu.add_command(label = "Новый")
@@ -150,11 +341,27 @@ def main():
     main_menu.add_cascade(label = "Файл", menu = file_menu)
     main_menu.add_cascade(label = "Справка", menu = help_menu)
     #endregion
+    
 
-    label_test.grid(row = 14, column = 0, padx = 5,)
-    print(root.grid_size()) 
+    #Упаковка нижнего меню
+    #region down_menu_gird
+    l_model_path.grid(row = 15, column = 1, columnspan = 9, sticky=W) 
+    e_model_path.grid(row = 16, column = 1, columnspan = 9, sticky=W)
+    b_model_path.grid(row = 16, column = 10, sticky=W+N)
+    l_json_path.grid(row = 17, column = 1, columnspan = 9, sticky=W ) #padx = 5
+    e_json_path.grid(row = 18, column = 1, columnspan = 9, sticky=W ) 
+    b_json_path.grid(row = 18, column = 10, sticky=W+N)
+    b_load_model.grid(row = 19, column = 1, columnspan = 9)
+    t_console.grid(row = 15, column = 12, columnspan = 5, rowspan = 5) 
+    scroll_console.grid(row = 15, column = 20, rowspan = 5, sticky=N+S) 
+    #endregion
+
+    print(root.grid_size()) # Отладка
+
 
     root.mainloop()
+
+
     
 
 
@@ -168,23 +375,58 @@ if __name__ == '__main__':
 
 
 #------TEMP------#
+
+#---------TRANING------------#
 '''
-file_name = fd.askopenfilename()
-print(file_name)
-img1 = Image.open(file_name)
-#img = ImageTk.PhotoImage(Image.open(file_name))
-img1 = img1.resize((400, 400), Image.ANTIALIAS)#
-img = ImageTk.PhotoImage(img1)    
-l_panel['image'] = img
+from imageai.Detection.Custom import DetectionModelTrainer
+trainer = DetectionModelTrainer()
+trainer.setModelTypeAsYOLOv3()
+trainer.setDataDirectory(data_directory="sign")
+trainer.setTrainConfig(object_names_array=["sign"], batch_size=2, num_experiments=8, train_from_pretrained_model="pretrained-yolov3.h5")
+trainer.trainModel()
 '''
 
-#img = Image.open(file_name)
-#img.show()
+#---------MARK-----------#
+'''
+trainer = DetectionModelTrainer()
+trainer.setModelTypeAsYOLOv3()
+trainer.setDataDirectory(data_directory="sign")
+metrics = trainer.evaluateModel(model_path="sign/models", json_path="sign/json/detection_config.json", iou_threshold=0.5, object_threshold=0.3, nms_threshold=0.5)
+print(metrics)
+'''
+#------------DETECTED-----------#
+'''
+from imageai.Detection.Custom import CustomObjectDetection
 
-#canvas = Canvas(root,width=150,height=150) 
+detector = CustomObjectDetection()
+detector.setModelTypeAsYOLOv3()
+detector.setModelPath("detection_model-ex-008--loss-0003.909.h5")
+detector.setJsonPath("detection_config.json")
+detector.loadModel()
+detections = detector.detectObjectsFromImage(input_image="image (68).jpg", output_image_path="image (68)-detected.jpg", minimum_percentage_probability=10)
+for detection in detections:
+    print(detection["name"], " : ", detection["percentage_probability"], " : ", detection["box_points"])
+'''
 
-#pilImage = Image.open(file_name)
-#image = ImageTk.PhotoImage(pilImage)
-#imagesprite = canvas.create_image(400,400,image=image)
+#------------TEMP-----------#
+'''
+file_name3 = fd.askopenfilename()
+detections = detector.detectObjectsFromImage(input_image=file_name3, output_image_path="image000.jpg", minimum_percentage_probability=10)
+print(detections)
+    
+#for detection in detections:
+    #print(detection["name"], " : ", detection["percentage_probability"], " : ", detection["box_points"])
 
-#canvas.pack()
+PIL_img = Image.open("image000.jpg")
+
+width_img = PIL_img.width
+height_img = PIL_img.height
+
+new_size = calculation_of_new_sizes(width_img, height_img, WLABEL, HLABEL)
+width_new = new_size[0]
+height_new = new_size[1]
+
+PIL_img = PIL_img.resize((width_new, height_new), Image.ANTIALIAS)
+TK_img = ImageTk.PhotoImage(PIL_img) 
+l_panel_processed['image'] = TK_img
+'''
